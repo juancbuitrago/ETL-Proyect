22b5b33cc0bb84ae35eef721a7b5581e96320cf2
VanessaSuare <dayanna.suarez@uao.edu.co>
2024-03-08 22:42:28 -0500

    structure organization update

--- config/configuration.py
+++ config/configuration.py
@@ -5,13 +5,12 @@ from configparser import ConfigParser
 def config(filename="database.ini", section="postgresql"):
     """create a parser"""
     parser = ConfigParser()
-    # read config file
     parser.read(filename)
     db = {}
     if parser.has_section(section):
         params = parser.items(section)
         for param in params:
             db[param[0]] = param[1]
+        return db
     else:
-        raise ValueError(f'{0} not found {1} file.'.format(section, filename))
-    return db
+        raise ImportError(f"Section '{section}' not found in the '{filename}' file")
